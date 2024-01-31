be458f063b620f114779b55d450c9c89b1a63e69
Linton <300087594@fvtc.edu>
2024-01-30 18:48:20 -0600

    Added Welcome Div

--- site.js
+++ site.js
@@ -1,1 +1,17 @@
-alert('It works!')
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+if (isMorning){
+ document.querySelector('#welcome').textContent = 'Good Morning'
+}
+
+if (isAfternoon){
+    document.querySelector('#welcome').textContent = 'Good Afternoon'
+   }
+
+if (isEvening){
+    document.querySelector('#welcome').textContent = 'Good Evening'
+   }
